c4dbc4d8ef383bc7db4396977c70f6798b619783
jarsp <jarsp.ctf@gmail.com>
2019-01-12 10:46:23 -0500

    fix

--- setup.sh
+++ setup.sh
@@ -104,6 +104,7 @@ sudo apt-get install -y \
 sudo apt-get install --install-recommends -y winehq-staging
 
 # RVM/Ruby install
+sudo apt-get update
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable --rails
 
